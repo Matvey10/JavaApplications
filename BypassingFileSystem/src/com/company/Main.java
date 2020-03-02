8fe2cad7a72d7189c49ed2c27a09335e4851f366
Matvey Vasilev <matt-14@mail.ru>
2020-03-02 17:59:31 +0300

    add command line arguments

--- BypassingFileSystem/src/com/company/Main.java
+++ BypassingFileSystem/src/com/company/Main.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        /*Scanner in = new Scanner(System.in);
-        String path = in.nextLine();
-        String regex = in.nextLine();
-        FileSystemWalker fileSystemWalker = new FileSystemWalker(path);*/
-        Test1();
-        Test2();
+        try{
+            Test3(args[0], args[1]);
+            Test4(args[0]);
+        }
+        catch (Exception e){
+            System.out.print("wrong input");
+        }
     }
     public static void Test1(){
         FileSystemWalker fileSystemWalker = new FileSystemWalker("C:\\Users\\user\\Desktop\\A", "[A-Z]1");
     }
     public static void Test2(){
         FileSystemWalker fileSystemWalker = new FileSystemWalker("C:\\Users\\user\\Desktop\\A");
+    }
+    public static void Test3(String dir, String reg) {
+        FileSystemWalker fileSystemWalker = new FileSystemWalker(dir, reg);
+    }
+    public static void Test4(String dir){
+        FileSystemWalker fileSystemWalker = new FileSystemWalker(dir);
     }
 }
